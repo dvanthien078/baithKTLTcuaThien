a9801841cb178e66ec509c4b135c2d9696a7350a
dvanthien078 <dvanthien078@gmail.com>
2024-12-13 08:21:26 +0700

    Create 4.py

---
+++ baith3/4.py
@@ -0,0 +1,9 @@
+print('đinh văn thiện msv 235752021610032')
+a="Hello teacher The Anh!"
+def say(a):
+    a="Vinh University"
+    print (a)
+    return(a)
+a="Hello teacher The Anh!"
+say (a)
+print (a)
