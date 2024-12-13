a2d3a9b2551cd94ed00ea59b83bfcf4893e17957
dvanthien078 <dvanthien078@gmail.com>
2024-12-13 08:23:08 +0700

    Create 7.py

---
+++ baith3/7.py
@@ -0,0 +1,8 @@
+print('đinh văn thiện 235752021610032')
+n=int (input ("nhập vào một số tự nhiên"))
+def checkValue (n) :
+    if n%2==0:
+      print ("Đây là một số chẵn")
+    else:
+      print ("Đây là một số lẻ")
+checkValue (n)
