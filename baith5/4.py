b8ec95d58941e85724aef4b660d289fef5f7269c
dvanthien078 <dvanthien078@gmail.com>
2024-12-13 08:08:10 +0700

    Create 4.py

---
+++ baith5/4.py
@@ -0,0 +1,10 @@
+print ("đinh văn thiện")
+print ("235752021610032")
+
+array = list(range(12, 38))
+
+print("Mảng ban đầu:", array)
+
+reversed_array = array[::-1]
+
+print("Mảng đã đảo ngược:", reversed_array)
