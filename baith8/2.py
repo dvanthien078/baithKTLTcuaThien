0167be7b8b3261b851e66d7726a01ed6004f32e6
dvanthien078 <dvanthien078@gmail.com>
2024-12-13 07:52:30 +0700

    Create 2.py

---
+++ baith8/2.py
@@ -0,0 +1,11 @@
+print('đinh văn thiện msv 235752021610032')
+import turtle
+window = turtle.Screen()
+window.bgcolor('black')
+thinh = turtle.Turtle()
+thinh.pencolor('white')
+for _ in range(20):
+    thinh.forward(150)
+    thinh.right(111)
+
+turtle.done()
